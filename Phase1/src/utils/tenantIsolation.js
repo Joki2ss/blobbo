57cf0b6c9828299d55c3e1f1851f5cf087d7fef3
Joki2ss <tsewangzangpo6221@gmail.com>
2026-01-12 04:43:05 +0100

    chore: pulizia root Phase1, rimosse cartelle/file non compatibili Snack Expo

--- Phase1/src/utils/tenantIsolation.js
+++
@@ -1,5 +0,0 @@
-// Utility for tenant isolation (stub, ready for backend integration)
-export function getTenantId(user) {
-  // Assumes user object has tenant_id property from backend
-  return user?.tenant_id || null;
-}
--- Phase1/src/utils/uuid.js
+++
@@ -1,4 +0,0 @@
-// Utility for validating Supabase UUIDs
-export function isValidUUID(id) {
-  return /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(id);
-}
