ae9f5aa0096c9d5b5ba6ff961785dccc9dc53370
Joki2ss <tsewangzangpo6221@gmail.com>
2026-01-13 17:47:49 +0100

    fix: App.js mostra direttamente dashboard mockata, compatibilità Snack garantita

--- App.js
+++ App.js
@@ -1,37 +1,6 @@
-import "./components/store/AppStore";
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { AppProviders } from "./components/store/AppStore";
-import { useAppState } from "./components/store/AppStore";
-import { RootNavigator } from "./components/navigation/RootNavigator";
-import { ThemeProvider, useTheme } from "./components/theme";
-import { SupportErrorBoundary } from "./components/support/SupportUI/SupportErrorBoundaryWrapper";
-import { GlobalUpdateGate } from "./components/components/GlobalUpdateGate";
-
-function ThemedApp() {
-	const theme = useTheme();
-	return (
-		<>
-			<StatusBar style={theme.isDark ? "light" : "dark"} />
-			<SupportErrorBoundary>
-				<GlobalUpdateGate />
-				<RootNavigator />
-			</SupportErrorBoundary>
-		</>
-	);
-}
-
-function ThemeBridge({ children }) {
-	const { themeMode } = useAppState();
-	return <ThemeProvider mode={themeMode}>{children}</ThemeProvider>;
-}
+import { DashboardScreen } from "./components/screens/app/DashboardScreen";
 
 export default function App() {
-	return (
-		<AppProviders>
-			<ThemeBridge>
-				<ThemedApp />
-			</ThemeBridge>
-		</AppProviders>
-	);
+	return <DashboardScreen />;
 }
