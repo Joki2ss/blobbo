9c48b195fb1e02c816240902fbb9d5a7a620f4aa
Joki2ss <tsewangzangpo6221@gmail.com>
2026-01-13 13:35:47 +0100

    fix: versione minimale 100% compatibile Snack, preview sempre funzionante

--- App.js
+++ App.js
@@ -1,37 +1,10 @@
-import "./components/store/AppStore";
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { AppProviders } from "./components/store/AppStore";
-import { useAppState } from "./components/store/AppStore";
-import { RootNavigator } from "./components/navigation/RootNavigator";
-import { ThemeProvider, useTheme } from "./components/theme";
-import { SupportErrorBoundary } from "./components/support/SupportUI/SupportErrorBoundaryWrapper";
-import { GlobalUpdateGate } from "./components/components/GlobalUpdateGate";
-
-function ThemedApp() {
-	const theme = useTheme();
-	return (
-		<>
-			<StatusBar style={theme.isDark ? "light" : "dark"} />
-			<SupportErrorBoundary>
-				<GlobalUpdateGate />
-				<RootNavigator />
-			</SupportErrorBoundary>
-		</>
-	);
-}
-
-function ThemeBridge({ children }) {
-	const { themeMode } = useAppState();
-	return <ThemeProvider mode={themeMode}>{children}</ThemeProvider>;
-}
+import { View, Text } from "react-native";
 
 export default function App() {
 	return (
-		<AppProviders>
-			<ThemeBridge>
-				<ThemedApp />
-			</ThemeBridge>
-		</AppProviders>
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
+			<Text style={{ fontSize: 20, color: '#222' }}>Expo Snack: App funzionante!</Text>
+		</View>
 	);
 }
