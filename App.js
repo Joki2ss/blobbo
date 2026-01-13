2b51d3c5d5c40e2b74d4b257148be29948ef35a7
Joki2ss <tsewangzangpo6221@gmail.com>
2026-01-13 06:10:54 +0100

    fix: rimosso LAB/App.js, ora tutta la logica in App.js root, nessun doppio import

--- App.js
+++ App.js
@@ -1,1 +1,37 @@
-export { default } from "./components/LAB/App";
+import "./components/store/AppStore";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { AppProviders } from "./components/store/AppStore";
+import { useAppState } from "./components/store/AppStore";
+import { RootNavigator } from "./components/navigation/RootNavigator";
+import { ThemeProvider, useTheme } from "./components/theme";
+import { SupportErrorBoundary } from "./components/support/SupportUI/SupportErrorBoundaryWrapper";
+import { GlobalUpdateGate } from "./components/components/GlobalUpdateGate";
+
+function ThemedApp() {
+	const theme = useTheme();
+	return (
+		<>
+			<StatusBar style={theme.isDark ? "light" : "dark"} />
+			<SupportErrorBoundary>
+				<GlobalUpdateGate />
+				<RootNavigator />
+			</SupportErrorBoundary>
+		</>
+	);
+}
+
+function ThemeBridge({ children }) {
+	const { themeMode } = useAppState();
+	return <ThemeProvider mode={themeMode}>{children}</ThemeProvider>;
+}
+
+export default function App() {
+	return (
+		<AppProviders>
+			<ThemeBridge>
+				<ThemedApp />
+			</ThemeBridge>
+		</AppProviders>
+	);
+}
